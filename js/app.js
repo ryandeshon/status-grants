19cda36f82a4e4de6323754f9700b2392e074010
Patrick Lee <me@patricklee.nyc>
2018-01-27 14:59:16 -0500

    Add FAQ and terms sections

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 (function ($) {
-  $(document).on('scroll', function (evt) {
+  $(document).on('scroll', function () {
     var target = $('.navigation');
     if ($(this).scrollTop() > 400) {
       target.removeClass('navigation__background--top')
@@ -7,6 +7,39 @@
     } else {
       target.removeClass('navigation__background--scroll');
       target.addClass('navigation__background--top')
+    }
+  })
+
+  $('.apply-btn').on('click', function (e) {
+    e.preventDefault()
+    $('html, body').animate({
+      scrollTop: $("#apply").offset().top
+    }, 2000);
+  })
+
+  $('.faq-btn').on('click', function (e) {
+    e.preventDefault()
+    $('html, body').animate({
+      scrollTop: $("#faq").offset().top
+    }, 2000);
+  })
+
+  $('.question__block').on('click', function (e) {
+    var toggle = $(this).data('toggle')
+    console.log(toggle)
+    if (toggle) {
+      $(this).data('toggle', false)
+      $(this).find('.question__block--body').hide()
+      $(this).find('.question__block--carat').removeClass('flip')
+      $(this).find('.question__block--carat').addClass('flip-reverse')
+    } else {
+      $(this).data('toggle', true)
+      $(this).find('.question__block--body').show()
+      $(this).find('.question__block--carat').removeClass('flip-reverse')
+      $(this).find('.question__block--carat').addClass('flip')
+      $('html, body').animate({
+        scrollTop: $(this).offset().top - 200
+      }, 1000);
     }
   })
 })(jQuery);
