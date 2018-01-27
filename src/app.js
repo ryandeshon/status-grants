ac2e911731dd0faf3a097e0ee0ebff29b91d64fd
Patrick Lee <me@patricklee.nyc>
2018-01-26 22:13:27 -0500

    Add webpack to bundle assets.

---
+++ src/app.js
@@ -0,0 +1,4 @@
+import scss from './scss/main.scss'
+import $ from 'jquery'
+window.jQuery = $;
+window.$ = $;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+
+
+var path = require('path')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+  filename: "[name].[contenthash].css",
+  disable: process.env.NODE_ENV === "development"
+});
+
+module.exports = {
+  entry: './src/app.js',
+  output: {
+    filename: 'application.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: extractSass.extract({
+          use: [{
+            loader: "css-loader"
+          }, {
+            loader: "sass-loader"
+          }],
+          // use style-loader in development
+          fallback: "style-loader"
+        })
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Status | The Mobile Ethereum Client',
+      template: 'src/index.html'
+    }),
+    extractSass
+  ]
+}
+
