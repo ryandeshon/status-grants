9dde28ce3fe5799faf5c9a2038563066069d9b39
Patrick Lee <me@patricklee.nyc>
2018-01-26 23:08:04 -0500

    Add font config

--- src/app.js
+++ src/app.js
@@ -2,3 +2,6 @@ import scss from './scss/main.scss'
 import $ from 'jquery'
 window.jQuery = $;
 window.$ = $;
+
+
+require('./assets/fonts/font-postgrotesk/postgrotesk.scss');
--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,10 @@ module.exports = {
         query: {
           presets: ['es2015']
         }
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'file-loader?name=fonts/[name].[ext]'
       }
     ]
   },
