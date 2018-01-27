fa9b9f963a131f0762ec82b49c3309331164c5c1
Patrick Lee <me@patricklee.nyc>
2018-01-27 12:33:33 -0500

    Add call to action and partnership blocks

--- js/app.js
+++ js/app.js
@@ -1,3 +1,12 @@
 (function ($) {
-  console.log('hello world')
+  $(document).on('scroll', function (evt) {
+    var target = $('.navigation');
+    if ($(this).scrollTop() > 400) {
+      target.removeClass('navigation__background--top')
+      target.addClass('navigation__background--scroll');
+    } else {
+      target.removeClass('navigation__background--scroll');
+      target.addClass('navigation__background--top')
+    }
+  })
 })(jQuery);
